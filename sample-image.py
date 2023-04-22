67d159d8cc5e8c6ece78ac8400f05787f116b376
RYNOLIPHANT <rynoliphant@u.boisestate.edu>
2023-04-22 12:01:33 -0600

    sample-image.py now outputs the method used for coating

--- sample-image.py
+++ sample-image.py
@@ -7,7 +7,15 @@ filename = sys.argv[1]
 data = np.loadtxt(filename, delimiter=' ')
 
 
+count=0
+f = open(filename, 'r')
+for line in f:
+	count+=1
+	if count == 6:
+		method_type = line.split()[3]
+		print('method is',method_type)
+
 plt.imshow(data)
-plt.title('Sample ' + filename[9:12])
+plt.title('Sample ' + filename[9:12]+' Where Method is '+str(method_type))
 plt.savefig('test-image'+filename[9:12]+'.png')
 plt.show()
