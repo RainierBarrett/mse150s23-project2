ecd7d34d2fda27ccf1782954aee6b21d7f98b43c
RYNOLIPHANT <rynoliphant@u.boisestate.edu>
2023-04-22 12:23:18 -0600

    Python script can now handle multiple files and has an error message in case the save location does not exist

--- sample-image.py
+++ sample-image.py
@@ -2,20 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-filename = sys.argv[1]
+filenames = sys.argv[1:]
 
-data = np.loadtxt(filename, delimiter=' ')
+for filename in filenames:
+	data = np.loadtxt(filename, delimiter=' ')
 
 
-count=0
-f = open(filename, 'r')
-for line in f:
-	count+=1
-	if count == 6:
-		method_type = line.split()[3]
-		print('method is',method_type)
+	count=0
+	f = open(filename, 'r')
+	for line in f:
+		count+=1
+		if count == 6:
+			method_type = line.split()[3]
+			print('method is',method_type)
 
-plt.imshow(data)
-plt.title('Sample ' + filename[9:12]+' Where Method is '+str(method_type))
-plt.savefig('test-image'+filename[9:12]+'.png')
-plt.show()
+	try :
+		plt.savefig('images/'+str(method_type)+'/'+filename[9:12]+'.png')
+	except :
+		print('Error: File save location not found, Method type '+str(method_type)+' is not an available folder')
+	else:
+
+		plt.imshow(data)
+		plt.title('Sample ' + filename[9:12]+' Where Method is '+str(method_type))
+		plt.savefig('images/'+str(method_type)+'/'+filename[9:12]+'.png')
+		plt.show()
