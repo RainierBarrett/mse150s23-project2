3342844ab9d3d4e0574aa8bd524e0ad2fa25f543
RYNOLIPHANT <rynoliphant@u.boisestate.edu>
2023-04-22 10:33:02 -0600

    images now have titles and are saved. Created a folder to put images into

--- sample-image.py
+++ sample-image.py
@@ -6,5 +6,8 @@ filename = sys.argv[1]
 
 data = np.loadtxt(filename, delimiter=' ')
 
+
 plt.imshow(data)
+plt.title('Sample ' + filename[9:12])
+plt.savefig('test-image'+filename[9:12]+'.png')
 plt.show()
